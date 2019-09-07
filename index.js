4b7cba7978b9a32fceae2f6b59658c41c99613de
Stefan Vojvodic <stefan.vojvodic@gamecredits.com>
2019-09-07 02:08:14 +0200

    added uuid package

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 const path = require('path');
 const express = require('express');
+const uuid = require('uuidv4').default;
 
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
-function generateUUID() {
-  var d = new Date().getTime(); //Timestamp
-  var d2 = (performance && performance.now && performance.now() * 1000) || 0; //Time in microseconds since page-load or 0 if unsupported
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16; //random number between 0 and 16
-    if (d > 0) {
-      //Use timestamp until depleted
-      r = (d + r) % 16 | 0;
-      d = Math.floor(d / 16);
-    } else {
-      //Use microseconds since page-load if supported
-      r = (d2 + r) % 16 | 0;
-      d2 = Math.floor(d2 / 16);
-    }
-    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
-  });
-}
 
 const EVENTS = {
   INIT_ROOM: 'INIT_ROOM',
@@ -41,7 +24,7 @@ io.on('connection', function(socket) {
 
   socket.on(EVENTS.INIT_ROOM, function(user) {
     user = user;
-    const room = generateUUID();
+    const room = uuid();
 
     rooms.set(room, [user]);
 
